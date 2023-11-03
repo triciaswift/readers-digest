181b519a765a2bb891db7768733dd7bb0383a9ce
Tricia Swift <tricia.swift8@gmail.com>
2023-11-03 13:09:40 -0500

    Added BookView & CategoryView

--- digestapi/models/user.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class User(models.Model):
-    """Database model for tracking users"""
-
-    first_name = models.CharField(max_length=155)
-    last_name = models.CharField(max_length=155)
-    email = models.CharField(max_length=155)
